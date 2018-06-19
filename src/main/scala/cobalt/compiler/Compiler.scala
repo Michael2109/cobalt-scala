bdb88f5cbcc10d71f001e7df348c6031a7d4933b
Michael <michael.jhaywoods@gmail.com>
2018-06-19 15:09:24 +0100

    Fixed parsing of file data.

--- src/main/scala/cobalt/compiler/Compiler.scala
+++ src/main/scala/cobalt/compiler/Compiler.scala
@@ -1,10 +1,14 @@
 package cobalt.compiler
 
-import fastparse.noApi._
-import cobalt.ast.AST._
+import java.io.{BufferedOutputStream, File, FileOutputStream, PrintWriter}
 import java.nio.file.{Path, Paths}
 
-import cobalt.parser.{StatementParser, WsApi}
+import cobalt.ast.AST
+import cobalt.ast.AST.Module
+import cobalt.code_gen.CodeGen
+import cobalt.ir.IR.ModuleIR
+import cobalt.parser.StatementParser
+import fastparse.core.Parsed
 
 import scala.io.Source
 
@@ -14,27 +18,39 @@ class Compiler(commandLineOptions: Map[CommandLineOption, Any], classPath: Path,
 
   def compile()= {
 
-    val absolutePathsToCompile = pathsToCompile.map(path => Paths.get(classPath.toString, path.toString))
+    val absolutePathsToCompile: List[Path] = pathsToCompile.map(path => Paths.get(classPath.toString, path.toString))
 
-    val cobaltFiles = absolutePathsToCompile.map(x => Source.fromFile(x.toFile))
+    val cobaltFiles: List[String] = absolutePathsToCompile.map(x => Source.fromFile(x.toFile).mkString)
 
     // Parse them
     val asts = cobaltFiles.map(cobaltFile => {
       import scala.reflect.runtime.universe._
-      println(Literal(Constant(cobaltFile.mkString.replace("\r", ""))))
-      StatementParser.moduleParser.parse(cobaltFile.mkString.replace("\r", ""))
+      println(Literal(Constant(cobaltFile.replace("\r", ""))))
+      StatementParser.moduleParser.parse(cobaltFile.replace("\r", ""))
     })
 
-    // "package x.y.z\nclass ClassName\n  let x(): Int = 1\n        "
-
-    // "package x.y.z\nclass ClassName\n  let x(): Int = 1\n"
-    asts.foreach(ast => ast match {
-      case Parsed.Success(value, _) => println(value)
-      case Parsed.Failure(a, b, c) => println("Failed compiling: " + a + " : " + b + " : " + c)
+    val modules: List[Module] = asts.map(ast => ast match {
+      case Parsed.Success(value, _) => value
+      case Parsed.Failure(a, b, c) => throw new Exception("Failed compiling: " + a + " : " + b + " : " + c)
     })
+
+    // Process AST
+    val moduleIRs: List[ModuleIR] = modules.map(AST.moduleToModuleIR)
 
     // Generate code
+    val moduleBytecodes: List[Array[Byte]] = moduleIRs.map(CodeGen.genCode)
 
     // Save to destination directory
+
+    // Create class file
+    val file = new File(classPath.toAbsolutePath.resolve(outputDir).resolve(pathsToCompile(0)).toString.replaceFirst("[.][^.]+$", "") + ".class")
+    file.mkdirs()
+    file.createNewFile()
+
+    val bos = new BufferedOutputStream(new FileOutputStream(classPath.toAbsolutePath.resolve(outputDir).resolve(pathsToCompile(0)).toString.replaceFirst("[.][^.]+$", "") + ".class"))
+
+    bos.write(moduleBytecodes(0))
+    bos.close()
+
   }
 }
